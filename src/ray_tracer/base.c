f44129dfbbbbe3652e06092f3998a7aa57d76d86
tom <tom@t-m1.local>
2022-07-04 23:14:22 +0200

    we got a circle :); need to adjust fov

--- src/ray_tracer/base.c
+++ src/ray_tracer/base.c
@@ -1,18 +1,44 @@
 #include "../../includes/miniRT.h"
 
+static bool	ray_sphere(t_ray ray, t_sp_list *sphere)
+{
+	t_vector	o_minus_c;
+	float		p;
+	float		q;
+	float		discriminant;
+	float		dRoot;
+	
+	o_minus_c = vector_copy(vector_sub(ray.origin, sphere->center));
+	p = vector_dot(ray.direction, o_minus_c);
+	q = vector_dot(o_minus_c, o_minus_c) - (sphere->radius * sphere->radius);
+	discriminant = (p * p) - q;
+	if (discriminant < 0.0f)
+	{
+		return (false);
+	}
+	dRoot = sqrtf(discriminant);
+	float dist1 = -p - dRoot;
+	float dist2 = -p + dRoot;
+
+	if (discriminant > 1e-7)
+		return (true);
+	return (false);
+}
+
 static t_ray	create_ray(t_screen *screen, t_camera *cam, float x, float y)
 {
 	t_ray	ray;
 
-	ray.origin.x = (x * (2.0 / (float)screen->width) + cam->position.x) - 1.0;
-	ray.origin.y = 1.0 - y * (2.0 / (float)screen->height) + cam->position.y;
+	float	fov = cam->fov * M_PI / 180.0;
+	ray.origin.x = x * (2.0 * tanf(fov / 2.0) / (float)screen->width) + cam->position.x - 1.0;
+	ray.origin.y =  1.0 - y * (2.0 * tanf(fov / 2.0) / (float)screen->height) + cam->position.y;
+	// ray.origin.x = (x * (2.0 / (float)screen->width) + cam->position.x) - 1.0;
+	// ray.origin.y = 1.0 - y * (2.0 / (float)screen->height) + cam->position.y;
 	ray.origin.z = cam->position.z + 1;
-	// printf("Pixel [%.0f,%.0f]\n", x, y);
-	// debug_print_vector(ray.origin);
-	// printf("--------------------\n");
-	ray.direction.x = cam->direction.x;
-	ray.direction.y = cam->direction.y;
-	ray.direction.z = cam->direction.z;
+	printf("Pixel [%.0f,%.0f]\n", x, y);
+	debug_print_vector(ray.origin);
+	printf("--------------------\n");
+	ray.direction = vector_copy(cam->direction);
 	ray.t_min = T_MIN;
 	ray.t_max = T_MAX;
 	return (ray);
@@ -31,6 +57,10 @@ void	basic_ray(t_screen *screen, t_objects *objs)
 		while (x <= screen->width)
 		{
 			ray = create_ray(screen, objs->cam, x, y);
+			if (ray_sphere(ray, objs->sp_head))
+				mlx_put_pixel(screen->img, x, y, 0x00FFFF);
+			else
+				mlx_put_pixel(screen->img, x, y, 0x0000FF);
 			x++;
 		}
 		y++;
